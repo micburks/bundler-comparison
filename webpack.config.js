bf81da51a55647ba32a56baad02eeb0daf99fea5
Mickey Burks <brks.mck@gmail.com>
2020-11-30 16:49:48 -0600

    Use webpack persistent cache

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
+const path = require('path');
 module.exports = {
-	entry: './index.js',
-	output: {
-		path: require('path').resolve('results'),
-		filename: 'webpack.js',
-		libraryTarget: 'commonjs'
-	},
-	devtool: 'source-map',
-	mode: 'production'
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: require('path').resolve('results'),
+    filename: 'webpack.js',
+    libraryTarget: 'commonjs'
+  },
+  devtool: 'source-map',
+  cache: {
+    type: 'filesystem',
+  },
 };
